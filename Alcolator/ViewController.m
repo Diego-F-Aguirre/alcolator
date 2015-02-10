6577b5cbe73111bfa4bf5c3104fc2b759460981e
Diego Aguirre <diego3884@gmail.com>
2015-02-09 22:28:18 -0500

    Initial Commit

---
+++ Alcolator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alcolator
+//
+//  Created by Diego Aguirre on 2/9/15.
+//  Copyright (c) 2015 Diego Aguirre. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
